ee24ef1118bc24cd92e0361d2801e39f7aea5ef9
Ben <Bendelathouwer@gmail.com>
2017-11-20 20:39:34 +0100

    readme
    changed the word document to a mark down file

--- arduino_datastream_via_wifi/config.h
+++ arduino_datastream_via_wifi/config.h
@@ -2,13 +2,13 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME    "xxxxx"
-#define IO_KEY         "xxxxx"
+#define IO_USERNAME    "BenDeLathouwer"
+#define IO_KEY         "0273a4c6c5c6408e8f8782fb0c8f5863"
 
 
 
-#define WIFI_SSID       "your_ssid"
-#define WIFI_PASS       "your_pass"
+#define WIFI_SSID       "The Hefty Network"
+#define WIFI_PASS       "0208196700"
 
 // comment out the following two lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
