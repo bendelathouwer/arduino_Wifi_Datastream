c44766fea5761fd916157d567f33245b6d2328f9
Ben <Bendelathouwer@gmail.com>
2017-11-21 21:05:30 +0100

    minor change
    made a minor change tot the code

--- arduino_datastream_via_wifi/config.h
+++ arduino_datastream_via_wifi/config.h
@@ -2,13 +2,13 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME    "BenDeLathouwer"
-#define IO_KEY         "0273a4c6c5c6408e8f8782fb0c8f5863"
+#define IO_USERNAME    "xxxxxxxxx"
+#define IO_KEY         "xxxxxxxxx"
 
 
 
-#define WIFI_SSID       "The Hefty Network"
-#define WIFI_PASS       "0208196700"
+#define WIFI_SSID       "xxxxxxxxxxx"
+#define WIFI_PASS       "xxxxxxxxxxxxxxxx"
 
 // comment out the following two lines if you are using fona or ethernet
 #include "AdafruitIO_WiFi.h"
